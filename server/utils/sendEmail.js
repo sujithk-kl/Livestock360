9d96044595bb349d2a6c2010fd6a13a611f3357a
sujithkumaravel <120175678+sujithkumaravel@users.noreply.github.com>
2026-02-12 09:31:35 +0530

    .

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,7 +6,8 @@ const sendEmail = async (options) => {
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        family: 4 // Force IPv4
     });
 
     const message = {
